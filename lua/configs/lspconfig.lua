93f3367ca25fc4744ec2cb27df2c597f1244c5bf
Jason Weaver <indyplanets@gmail.com>
2024-10-03 21:26:46 -0500

    update for html, css, prettierd support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -7,14 +7,13 @@ local lspconfig = require("lspconfig")
 -- list of all servers configured.
 lspconfig.servers = {
     "lua_ls",
-    -- "clangd",
-    -- "gopls",
-    -- "pyright",
 }
 
 -- list of servers configured with default config.
 local default_servers = {
-    -- "pyright",
+    "ts_ls",
+    "html",
+    "cssls",
 }
 
 -- lsps with default config
@@ -25,39 +24,6 @@ for _, lsp in ipairs(default_servers) do
         capabilities = capabilities,
     })
 end
-
--- lspconfig.clangd.setup({
---     on_attach = function(client, bufnr)
---         client.server_capabilities.documentFormattingProvider = false
---         client.server_capabilities.documentRangeFormattingProvider = false
---         on_attach(client, bufnr)
---     end,
---     on_init = on_init,
---     capabilities = capabilities,
--- })
-
--- lspconfig.gopls.setup({
---     on_attach = function(client, bufnr)
---         client.server_capabilities.documentFormattingProvider = false
---         client.server_capabilities.documentRangeFormattingProvider = false
---         on_attach(client, bufnr)
---     end,
---     on_init = on_init,
---     capabilities = capabilities,
---     cmd = { "gopls" },
---     filetypes = { "go", "gomod", "gotmpl", "gowork" },
---     root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
---     settings = {
---         gopls = {
---             analyses = {
---                 unusedparams = true,
---             },
---             completeUnimported = true,
---             usePlaceholders = true,
---             staticcheck = true,
---         },
---     },
--- })
 
 lspconfig.lua_ls.setup({
     on_attach = on_attach,
